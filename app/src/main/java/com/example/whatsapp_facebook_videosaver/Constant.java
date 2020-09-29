c5bed43b61472cd51107c3b87219eb4148c09805
Samran Tariq <itssamrantariq@gmail.com>
2020-09-28 21:06:00 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/whatsapp_facebook_videosaver/Constant.java
@@ -0,0 +1,14 @@
+package com.example.whatsapp_facebook_videosaver;
+
+/**
+ * Created by Tushar on 9/10/2017.
+ */
+
+class Constant {
+    public static int nav_clicked = 0;
+    public static Boolean isNavClicked = false;
+
+    public static Boolean isToggle = true;
+    public static int color = 0xff3b5998;
+    public static int theme = R.style.AppTheme;
+}
